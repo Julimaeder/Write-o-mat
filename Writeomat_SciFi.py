f9c7d3fed613903558a3887fef29d1472341c560
Fatma Cetinkaya <115086500+ftmct@users.noreply.github.com>
2022-12-21 18:14:49 +0100

    Create Writeomat_SciFi.py

--- Writeomat_SciFi.py
+++ Writeomat_SciFi.py
@@ -3,11 +3,13 @@
 from Writeomat import Geschichte
 Name_Held = input("Wähle den Namen deines Protagonisten \n-->")
 
+SciFi_Geschichte_1 = Geschichte("Wähle den Helden deiner Geschichte ",
+           "a) Super Held \nb) Bösewicht",
+           f"Es war einmal in einer fernen Galaxie ein Held {Name_Held}. Dieser ",
+           f"Es war einmal in einer fernen Galaxie ein Bösewicht namens {Name_Held} the destroyer. Dieser ")
 
-SciFi_Geschichte_1 = Geschichte("Info Text SciFi",
-           "Optionen SciFi",
-           f"option a SciFi{Name_Held}",
-           f"option b SciFi{Name_Held}",
-           f"option c SciFi{Name_Held}",
-           f"option d SciFi{Name_Held} ")
+SciFi_Geschichte_2 = Geschichte("An welchem Ort willst du starten",
+           "a) Raumschiff \nb) Planet Auria",
+           "Er war in seinem riesen Raumschiff mit dem er durch die Galaxie reiste. ",
+           "Er landete auf dem Planeten Auria, welcher bekannt für seine freundlichen Bewohner ist. ")
 
