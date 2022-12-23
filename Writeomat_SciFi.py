6051e76d85e6f36036e6a5c802ddbb1dea0d9c03
Julimaeder <maeder.julian@gmail.com>
2022-12-23 11:52:31 +0100

    Titelblatt-Bilder hinzugefügt

--- Writeomat_SciFi.py
+++ Writeomat_SciFi.py
@@ -10,19 +10,63 @@ SciFi_Geschichte_1 = Geschichte_2("Wähle den Helden deiner Geschichte ",
 
 SciFi_Geschichte_2 = Geschichte_2("An welchem Ort willst du starten",
            "a) Raumschiff \nb) Planet Auria",
-           "Er war in seinem riesen Raumschiff mit dem er durch die Galaxie reiste. ",
-           "Er landete auf dem Planeten Auria, welcher bekannt für seine freundlichen Bewohner ist. ")
+           "war in seinem riesen Raumschiff mit dem er durch die Galaxie reiste. ",
+           "landete auf dem Planeten Auria, welcher bekannt für seine freundlichen Bewohner ist. ")
 
 def Titelblatt(SciFi_Geschichte_1):
     if "Held" in SciFi_Geschichte_1:
         return(
-        """                                                                                                      
+        """
+
+                                              @@@@                              
+                    #@@@@@@@@@@               @@@@                              
+            (%        @@@@@@@@@@              @@@@                              
+#%%%%%%#*.(%%%%%(.*#%%%%( ,# @@             &%@@@@%%%#%%%%%%/                   
+    %%%%%%%%%%%%%%%%%%%%%%%%@@@#          .@@*//,,@@@@@@%%%%%%%%%%%%%%%%%%#     
+      #%%%%%%%%%%%%     @@@@@@%@@         @@@@,@@@@@@@@@%%%%%%%%%%%%%%%%%       
+                        @@ @@@@@@        @@@ @@@@@@& @@@%%%%%%%%%%%%%%%%        
+                       &@#  @@@@@       @@@* @@@@@@  &@@@%%%%%%%%%%%#*          
+                        @/ @@@@@@@      @@  &@@@@@@,  @@#.%%%%%%%%%%%           
+                        @#@&&&&@@&,     @&  @&@&@&&@ @@@   %%%%%%%%.            
+                        (@@@@@@@@@       @ (@@@ @@@@@@@     %%%                 
+                        &@@@  @@@@          @@@  @@@@        %(                 
+                        @@@   @@@           @@#   *@@                           
+                       #@@    @@@           @@@    @@@#                         
+                      *@@@    @@@           @@@@   @@@@                         
+                      @@@     @@            @@(     (@@                         
+                      @@      @@           *@@       @@                         
+                     @@       @@@        @@@@@      @@@                         
+                     @                               @@                         
+
 
         """
                )
     elif "Bösewicht" in SciFi_Geschichte_1:
         return(
         """
+
+         .##.                                                    ,#(              
+           .@@@@(.                                          ,(@@@%.               
+             ,@@@@@@@(.                                .(@@@@@@@,                 
+               (@@@@@@@@@&/.                      .(&@@@@@@@@@/                   
+                 #@@@@@@@@@@@@%*             ./&@@@@@@@@@@@@#                     
+                                                                                  
+                                                                                  
+                                                                                  
+                        .(@@@@@@@@@@*    (@@@@@@@@@@/                             
+                            .#@@@@@@@@.*@@@@@@@@(                                 
+                                ,#@@@@@@@@@@(.                                    
+                                    ,#@@(.                                        
+                                                                                  
+                                                                                  
+                                                                                  
+                                                                                  
+.,,       ,,    ,,     ,,         .,.             ,,        ,,    .,,      ,,     
+ /@@    ,@@.   ,@@.    @@*        #@(            &@@@      .@@.   (@@@/   .@@     
+  *@&  ,@@.    ,@@.    @@*        #@(          .&@,*@&.    .@@.   (@%.&@* .@@     
+   *@&,@@.     ,@@.    @@*        #@(         .&@,  *@&.   .@@.   (@%  ,@@,@@     
+    ,@@&.      ,@@.    @@@@@@@&   #@@@@@@@,  .&@*    *@&.  .@@.   (@%    /@@@     
+
 
         """
                )
