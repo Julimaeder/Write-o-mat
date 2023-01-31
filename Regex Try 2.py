743167bbf85dfec4d1cf323d2ef02ffe4bf8f8a2
Ersan Kahraman <115080173+ErsanK61@users.noreply.github.com>
2023-01-31 09:43:46 +0100

    Dictionary Regex

---
+++ Regex Try 2.py
@@ -0,0 +1,26 @@
+from PyDictionary import PyDictionary
+
+def Geschlecht_Angepasst(text, Geschlecht):
+    if Geschlecht == 'M':
+        text = re.sub(r'\b(er)\b', 'er', text)
+        text = re.sub(r'\b(Ein)\b', 'Ein', text)
+    elif Geschlecht == 'W':
+        text = re.sub(r'\b(er)\b', 'sie', text)
+        text = re.sub(r'\b(Ein)\b', 'Eine', text)
+    else:
+        raise ValueError("Invalid gender option. Please choose 'M' for male or 'W' for female.")
+        
+    # Pronomen wchsel
+    text_komplett= text.split()
+    for i in range(len(text_komplett)):
+        word = text_komplett[i]
+        if dictionary.gender(word) == 'male':
+            if Geschlecht == 'W':
+                word = dictionary.feminine(word)
+                text_komplett[i] = word
+        elif dictionary.gender(word) == 'female':
+            if Geschlecht == 'M':
+                word = dictionary.masculine(word)
+                text_komplett[i] = word
+    neuer_text = ' '.join(text_komplett)
+    return neuer_text
